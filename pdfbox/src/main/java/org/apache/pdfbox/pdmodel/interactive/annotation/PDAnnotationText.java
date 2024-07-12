985471490e8adb088555d730487474ae67216b80
Ibrahim Wassouf <ib981347@dal.ca>
2024-07-12 20:35:32 -0300

    Fix: fix multifaceted abstraction by splitting to two classes

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationText.java
@@ -31,101 +31,13 @@ public class PDAnnotationText extends PDAnnotationMarkup
 {
     private PDAppearanceHandler customAppearanceHandler;
 
-    /*
-     * The various values of the Text as defined in the PDF 1.7 reference Table 172
-     */
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_COMMENT = "Comment";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_KEY = "Key";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_NOTE = "Note";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_HELP = "Help";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_NEW_PARAGRAPH = "NewParagraph";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_PARAGRAPH = "Paragraph";
-
-    /**
-     * Constant for the name of a text annotation.
-     */
-    public static final String NAME_INSERT = "Insert";
-
-    /**
-     * Constant for the name of a circle annotation.
-     */
-    public static final String NAME_CIRCLE = "Circle";
-
-    /**
-     * Constant for the name of a cross annotation.
-     */
-    public static final String NAME_CROSS = "Cross";
-    
-    /**
-     * Constant for the name of a star annotation.
-     */
-    public static final String NAME_STAR = "Star";
-
-    /**
-     * Constant for the name of a check annotation.
-     */
-    public static final String NAME_CHECK = "Check";
-
-    /**
-     * Constant for the name of a right arrow annotation.
-     */
-    public static final String NAME_RIGHT_ARROW = "RightArrow";
-
-    /**
-     * Constant for the name of a right pointer annotation.
-     */
-    public static final String NAME_RIGHT_POINTER = "RightPointer";
-
-    /**
-     * Constant for the name of a crosshairs annotation.
-     */
-    public static final String NAME_UP_ARROW = "UpArrow";        
-
-    /**
-     * Constant for the name of a crosshairs annotation.
-     */
-    public static final String NAME_UP_LEFT_ARROW = "UpLeftArrow";        
-
-    /**
-     * Constant for the name of a crosshairs annotation.
-     */
-    public static final String NAME_CROSS_HAIRS = "CrossHairs";        
-
-    /**
-     * The type of annotation.
-     */
-    public static final String SUB_TYPE = "Text";
-
+  
     /**
      * Constructor.
      */
     public PDAnnotationText()
     {
-        getCOSObject().setName(COSName.SUBTYPE, SUB_TYPE);
+        getCOSObject().setName(COSName.SUBTYPE, PDAnnotationTextEnum.SUB_TYPE);
     }
 
     /**
@@ -177,7 +89,7 @@ public class PDAnnotationText extends PDAnnotationMarkup
      */
     public String getName()
     {
-        return getCOSObject().getNameAsString(COSName.NAME, NAME_NOTE);
+        return getCOSObject().getNameAsString(COSName.NAME, PDAnnotationTextEnum.NAME_NOTE);
     }
 
     /**
